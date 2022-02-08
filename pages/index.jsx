b095c6befdabc73f508f42f675f0094b50d3fcdd
Scott-fo <scottfo@live.com>
2022-02-08 04:56:14 +0000

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Spotify 2.0</title>
+      </Head>
+
+      <main>
+        {/* Sidebar */}
+
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+      
+    </div>
+  )
+}
